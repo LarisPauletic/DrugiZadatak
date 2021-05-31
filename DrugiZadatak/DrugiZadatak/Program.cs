0d5bab55ec5a87e475afb059a1cac35823d4b45a
LarisPauletic <58471911+LarisPauletic@users.noreply.github.com>
2021-05-31 09:19:13 +0200

    Add files via upload

---
+++ DrugiZadatak/DrugiZadatak/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrugiZadatak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ukupnoSekundi;
+            int minute, sekunde;
+            string time;
+            Console.Write("Unesite sekunde: ");
+            ukupnoSekundi = Convert.ToInt32(Console.ReadLine());
+           
+            sekunde = ukupnoSekundi % 60;
+            minute = ukupnoSekundi / 60;
+             time = minute + ":" + sekunde;
+            Console.WriteLine(time);
+
+            Console.ReadKey();
+        }
+    }
+}
